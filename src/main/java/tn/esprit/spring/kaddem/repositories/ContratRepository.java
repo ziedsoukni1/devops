e89ddc132fc22af720a2629e28600d37b10934d0
ziedsoukni1 <74630025+ziedsoukni1@users.noreply.github.com>
2024-11-03 19:40:53 +0100

    .

--- src/main/java/tn/esprit/spring/kaddem/repositories/ContratRepository.java
+++ src/main/java/tn/esprit/spring/kaddem/repositories/ContratRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.spring.kaddem.entities.Contrat;
